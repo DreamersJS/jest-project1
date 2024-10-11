8fe391d0aa6f427a21ffabfd54267faa6aec2ed1
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-10-11 18:55:05 +0300

    fix jest config now it supports svg and css

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
-// jest.config.js
 export default {
     testEnvironment: 'jsdom',
     transform: {
       '^.+\\.(js|jsx)$': 'babel-jest',
     },
     moduleFileExtensions: ['js', 'jsx'],
-    setupFilesAfterEnv: ['./setupTests.js'], 
+    setupFilesAfterEnv: ['./setupTests.js'],
+    moduleNameMapper: {
+      '\\.(css|less|scss)$': 'identity-obj-proxy', // Mock CSS imports
+      '\\.(svg|png|jpg|jpeg|gif)$': '<rootDir>/__mocks__/fileMock.js', // Mock static files
+    },
   };
   
